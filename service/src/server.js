e6692ba185b07d0d8f82087f801cb613c8d3124c
Roie Cohen <roie.cohen@gmail.com>
2020-02-13 07:33:52 -0800

    cleanup

--- service/src/server.js
+++ service/src/server.js
@@ -7,12 +7,11 @@
 import express from 'express'
 // middleware to allow cross-origin requests
 import cors from 'cors'
-// middleware to support GraphQL
-import { ApolloServer } from 'apollo-server-express'
+
 // GraphQL schema compilation
 import { makeExecutableSchema } from 'graphql-tools'
 // Auth0 Authentication client
-import { AuthenticationClient } from 'auth0'
+
 // Keep GraphQL stuff nicely factored
 import glue from 'schemaglue'
 import path from 'path'
@@ -21,56 +20,31 @@ import fs from 'fs'
 import busboy from 'connect-busboy'
 // Internal imports
 //
-import {
-  log,
-  print,
-  initMetrics,
-  counter,
-  BuildGraphqlClient
-} from 'io.maana.shared'
-import { uploadToAzure, uploadStreamToBlob } from './azureUpload'
+import { log, print, initMetrics } from 'io.maana.shared'
+import { uploadStreamToBlob } from './azureUpload'
 require('dotenv').config()
 
-const options = {
-  mode: 'js' // default
-  // ignore: '**/somefileyoudonotwant.js'
-}
-const schemaPath = path.join(
-  '.',
-  `${__dirname}`.replace(process.cwd(), ''),
-  'graphql/'
-)
-const glueRes = glue(schemaPath, options)
+// const options = {
+//   mode: 'js' // default
+//   // ignore: '**/somefileyoudonotwant.js'
+// }
+// const schemaPath = path.join(
+//   '.',
+//   `${__dirname}`.replace(process.cwd(), ''),
+//   'graphql/'
+// )
+// const glueRes = glue(schemaPath, options)
 
-// Compile schema
-export const schema = makeExecutableSchema({
-  typeDefs: glueRes.schema,
-  resolvers: glueRes.resolver
-})
+// // Compile schema
+// export const schema = makeExecutableSchema({
+//   typeDefs: glueRes.schema,
+//   resolvers: glueRes.resolver
+// })
 
 const uploadPath = path.join(__dirname, './tmp/') // Register the upload path
 
 if (!fs.existsSync(uploadPath)) {
   fs.mkdirSync(uploadPath)
-}
-//
-// Client setup
-// - allow this service to be a client of Maana Q's Computational Knowledge Graph
-//
-let client
-const clientSetup = token => {
-  if (!client) {
-    // construct graphql client using endpoint and context
-    client = BuildGraphqlClient(CKG_ENDPOINT_URL, (_, { headers }) => {
-      // return the headers to the context so httpLink can read them
-      return {
-        headers: {
-          ...headers,
-          authorization: token ? `Bearer ${token}` : ''
-        }
-      }
-    })
-  }
 }
 
 //
@@ -88,15 +62,12 @@ const HOSTNAME = process.env.HOSTNAME || 'localhost'
 // External DNS name for service
 const PUBLICNAME = process.env.PUBLICNAME || 'localhost'
 
-// Remote (peer) services we use
-const CKG_ENDPOINT_URL = process.env.CKG_ENDPOINT_URL
-
 const app = express()
 //
 // CORS
 //
 const corsOptions = {
-  origin: `*`,
+  origin: '*',
   credentials: true // <-- REQUIRED backend setting
 }
 
@@ -138,41 +109,40 @@ app.route('/upload').post((req, res, next) => {
   })
 })
 
-const defaultSocketMiddleware = (connectionParams, webSocket) => {
-  return new Promise(function(resolve, reject) {
-    log(SELF).warn(
-      'Socket Authentication is disabled. This should not run in production.'
-    )
-    resolve()
-  })
-}
+// const defaultSocketMiddleware = (connectionParams, webSocket) => {
+//   return new Promise((resolve, reject) => {
+//     log(SELF).warn(
+//       'Socket Authentication is disabled. This should not run in production.'
+//     )
+//     resolve()
+//   })
+// }
 
 initMetrics(SELF.replace(/[\W_]+/g, ''))
-const graphqlRequestCounter = counter('graphqlRequests', 'it counts')
 
 const initServer = options => {
-  let { httpAuthMiddleware, socketAuthMiddleware } = options
+  //   let { httpAuthMiddleware, socketAuthMiddleware } = options
 
-  let socketMiddleware = socketAuthMiddleware || defaultSocketMiddleware
+  //   let socketMiddleware = socketAuthMiddleware || defaultSocketMiddleware
 
-  const server = new ApolloServer({
-    schema,
-    subscriptions: {
-      onConnect: socketMiddleware
-    }
-  })
+  //   const server = new ApolloServer({
+  //     schema,
+  //     subscriptions: {
+  //       onConnect: socketMiddleware
+  //     }
+  //   })
 
-  server.applyMiddleware({
-    app
-  })
+  //   server.applyMiddleware({
+  //     app
+  //   })
 
   const httpServer = http.createServer(app)
 
-  server.installSubscriptionHandlers(httpServer)
+  // server.installSubscriptionHandlers(httpServer)
 
   httpServer.listen({ port: PORT }, () => {
     log(SELF).info(
-      `listening on ${print.external(`http://${HOSTNAME}:${PORT}/graphql`)}`
+      `listening on ${print.external(`http://${HOSTNAME}:${PORT}`)}`
     )
   })
 
